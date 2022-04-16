6c783746b3ec1e7b7f8d038a4e47af1ba009a06a
Sinan Keskin <sinan@kesk.in>
2022-04-16 18:38:01 +0300

    Fix deprecation message for modifier

--- addon/modifiers/click-to-overlay.js
+++ addon/modifiers/click-to-overlay.js
@@ -1,7 +1,10 @@
 import { modifier } from 'ember-modifier';
 
-export default modifier(function clickToOverlay(element, [check] /*, hash*/) {
-  if (check) {
-    element.dataset.micromodalClose = true;
-  }
-});
+export default modifier(
+  (element, [check]) => {
+    if (check) {
+      element.dataset.micromodalClose = true;
+    }
+  },
+  { eager: false }
+);
